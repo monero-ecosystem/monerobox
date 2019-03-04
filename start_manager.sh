4ae1d37eaeb95de74f82a5bb62568ff7eca5621d
Jason Wong <jason.hcwong@gmail.com>
2019-03-04 18:21:54 +0100

    add volume for tor

--- start_manager.sh
+++ start_manager.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
 docker network create --subnet 172.20.1.0/24 --gateway 172.20.1.1 monerobox
+docker volume create data_tor
 docker volume create data_monero
 docker volume create settings
 
