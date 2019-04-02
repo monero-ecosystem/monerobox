98163458e65f80a1df56440effa924ef35fc9242
Jason Wong <jason.hcwong@gmail.com>
2019-04-02 22:07:19 +0800

    fix deps

--- manager_entrypoint.sh
+++ manager_entrypoint.sh
@@ -2,18 +2,26 @@
 
 set -ex
 
+memLimit_1gb="1508344"
+memLimit_2gb="3016688"
+memTotal=`grep MemTotal /proc/meminfo | awk '{print $2}'`
+
 cd /usr/src/app/monerobox
 
-# pull yml files and config files from github
+# get new yml and config files
 git pull
-cp settings/* /settings
+cp settings/*.yml /settings
+cp settings/monerod.conf.* /settings/
+cp settings/*.conf /settings/
 
+# copy service config file only if it does not exist
+if [ ! -f "/settings/service.json" ]; then
+  cp settings/service.json /settings/
+fi
+
+# link the monerod config file according to memory of the board, only if it does not exist
 if [ ! -f "/settings/monerod.conf" ]; then
-  # link the monerod config file according to memory of the board
   # For device with 1GB RAM: limit speed avoid oom-killer
-  memLimit_1gb="1508344"
-  memLimit_2gb="3016688"
-  memTotal=`grep MemTotal /proc/meminfo | awk '{print $2}'`
   if [ "$memTotal" -lt "$memLimit_1gb" ]; then
     ln -s /settings/monerod.conf.1gb /settings/monerod.conf
   elif [ "$memTotal" -lt "$memLimit_2gb" ]; then
@@ -27,20 +35,23 @@ while :
 do
   # get new yml and config files
   git pull
-  cp settings/* /settings
+  cp settings/*.yml /settings/
+  cp settings/monerod.conf.* /settings/
+  cp settings/*.conf /settings/
 
-  # pull images from dockerhub
+  # pull docker images
   /usr/local/bin/docker-compose -f /settings/tor.yml pull
   /usr/local/bin/docker-compose -f /settings/monerod.yml pull
   /usr/local/bin/docker-compose -f /settings/web.yml pull
   /usr/local/bin/docker-compose -f /settings/manager.yml pull
-  
-  # start all services
+
+  # start enabled services 
   /usr/local/bin/docker-compose -f /settings/tor.yml up -d
   /usr/local/bin/docker-compose -f /settings/monerod.yml up -d
   /usr/local/bin/docker-compose -f /settings/web.yml up -d
-  /usr/local/bin/docker-compose -f /settings/manager.yml up -d
+  # /usr/local/bin/docker-compose -f /settings/manager.yml up -d
 
   # sleep for 12 hours
   sleep 12h
 done
+
